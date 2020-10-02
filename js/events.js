91537e4932e9277808d75b269778fc789f6fb8f0
fixedbets <fixedbet@riseup.net>
2020-10-02 21:40:41 +0500

    react

---
+++ js/events.js
@@ -0,0 +1,142 @@
+var events = [
+  {
+    "eventId": 556,
+    "timestamp": 1601650800,
+    "sport": "Soccer",
+    "competition": "Romania. Liga 2.",
+    "eventName": "FC Rapid Bucuresti - Concordia",
+    "password": "IwdsKk",
+    "finished": false,
+    "walletNum": 0
+  },
+  {
+    "eventId": 555,
+    "timestamp": 1601643600,
+    "sport": "Soccer",
+    "competition": "Kazakhstan. Premier League.",
+    "eventName": "Tobol - Zhetysu Taldykorgan",
+    "password": "IwdsKk",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 554,
+    "timestamp": 1601575200,
+    "sport": "Soccer",
+    "competition": "Brazil. Serie D.",
+    "eventName": "Pelotas - SER Caxias",
+    "password": "5q4jVv",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 553,
+    "timestamp": 1601560800,
+    "sport": "Soccer",
+    "competition": "Sudan. Premier League.",
+    "eventName": "Ahli Shendi - Al-Ahly Merowe",
+    "password": "jZzYlU",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 552,
+    "timestamp": 1601546400,
+    "sport": "Soccer",
+    "competition": "Vietnam. V.League 1.",
+    "eventName": "Hai Phong - Song Lam Nghe An",
+    "password": "vDaSl4",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 551,
+    "timestamp": 1601488800,
+    "sport": "Soccer",
+    "competition": "Brazil. Serie D.",
+    "eventName": "Bragantino - Fast Clube",
+    "password": "gYfO4r",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 550,
+    "timestamp": 1601488800,
+    "sport": "Soccer",
+    "competition": "Morocco. Botola Pro.",
+    "eventName": "FUS Rabat - Rapide Oued Zem",
+    "password": "anJhLD",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 549,
+    "timestamp": 1601485200,
+    "sport": "Soccer",
+    "competition": "Poland. Division 1.",
+    "eventName": "Radomiak Radom - Legnica",
+    "password": "Wjth4U",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 548,
+    "timestamp": 1601442000,
+    "sport": "Soccer",
+    "competition": "Kazakhstan. First Division.",
+    "eventName": "FK Kyran - Maqtaaral",
+    "password": "YTr1WI",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 547,
+    "timestamp": 1601417700,
+    "sport": "Soccer",
+    "competition": "Brazil. Serie B.",
+    "eventName": "Vitoria - CSA",
+    "password": "2oTILT",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 546,
+    "timestamp": 1601398800,
+    "sport": "Soccer",
+    "competition": "Poland. Division 2.",
+    "eventName": "Pruszkow - Hutnik Krakow",
+    "password": "ZE55CJ",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 545,
+    "timestamp": 1601377200,
+    "sport": "Soccer",
+    "competition": "Georgia. Crystalbet Erovnuli Liga 2.",
+    "eventName": "WIT Georgia - Shevardeni Tbilisi",
+    "password": "T9AJsm",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 544,
+    "timestamp": 1601377200,
+    "sport": "Soccer",
+    "competition": "Georgia. Crystalbet Erovnuli Liga 2.",
+    "eventName": "Merani Martvili - Zugdidi",
+    "password": "sGGck4",
+    "finished": true,
+    "walletNum": 1
+  },
+  {
+    "eventId": 557,
+    "timestamp": 1601650800,
+    "sport": "Soccer",
+    "competition": "123",
+    "eventName": "FC Rapid Bucuresti - Concordia",
+    "password": "",
+    "finished": true,
+    "walletNum": 0
+  },
+]
---
+++ js/prices.js
@@ -0,0 +1,7 @@
+var prices = {
+  "bitcoin": {"usd" : 1},
+  "ethereum": {"usd" : 2},
+  "litecoin": {"usd" : 3},
+  "monero": {"usd" : 4},
+  "dash": {"usd" : 5}
+}
